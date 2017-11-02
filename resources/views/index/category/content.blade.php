8881824e05a2f13aaa91e2a2ecafe2d275044766
Nguyen Van Trong <nguyenwipwa@gmail.com>
2017-11-02 16:05:58 +0700

    Them font

--- resources/views/index/category/content.blade.php
+++ resources/views/index/category/content.blade.php
@@ -13,22 +13,8 @@
   <meta name="geo.placename" content="Hà nội" />
   <meta name="geo.position" content="20.984321;105.818546" />
   <meta name="ICBM" content="20.984321, 105.818546" />
-  <link rel="shortcut icon" href="http://fptcamera.vn/resources/uploads/images/automatic/favicon/camera-giam-sat-fpt.png" type="image/x-icon">
-  <link rel="stylesheet" type="text/css" href="http://fptcamera.vn/resources/stylesheets/client/bootstrap.css" />
-  <link rel="stylesheet" type="text/css" href="http://fptcamera.vn/resources/stylesheets/client/bootstrap-theme.css" />
-  <link rel="stylesheet" type="text/css" href="http://fptcamera.vn/resources/stylesheets/client/normalize.css" />
-  <link rel="stylesheet" type="text/css" href="http://fptcamera.vn/resources/stylesheets/client/app.css" />
-  <link rel="stylesheet" type="text/css" href="http://fptcamera.vn/resources/stylesheets/client/font-awesome.css" />
-  <link rel="stylesheet" type="text/css" href="http://fptcamera.vn/resources/stylesheets/client/flexslider.css" />
-
-  <script src="http://fptcamera.vn/resources/js/client/jquery-2.1.3.min.js"></script>
-  <script src="http://fptcamera.vn/resources/js/client/jquery-ui-1.9.2.custom.min.js"></script>
-  <script src="http://fptcamera.vn/resources/js/client/bootstrap.js"></script>
-  <script src="http://fptcamera.vn/resources/js/client/scrollfix.js"></script>
-  <script src="http://fptcamera.vn/resources/js/client/equalheight.js"></script>
-  <script src="http://fptcamera.vn/resources/js/client/jquery.flexslider-min.js"></script>
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic&subset=latin,vietnamese' rel='stylesheet' type='text/css'>
-  <link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic&subset=latin,vietnamese' rel='stylesheet' type='text/css'>
+  
+  @include("../../.link.index");
 
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -269,7 +255,7 @@
                             </div>
                             <div class="body">
                               @foreach ($menu_left as $element)
-                              <a class="sort_list" href="{{url('/category/'.$element->id)}}">{{$element->name}}</a>
+                              <a class="sort_list" href="{{url('/category/'.$element->id.'/1')}}">{{$element->name}}</a>
                               @endforeach
                             </div>
 
