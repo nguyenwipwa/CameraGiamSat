274348ff954d1378f2a42f444955167208adbdfb
Tin <pctnhock1@gmail.com>
2018-01-12 12:59:53 +0700

    sua linh tinh

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in! okkkkkkkkk
                 </div>
             </div>
         </div>
